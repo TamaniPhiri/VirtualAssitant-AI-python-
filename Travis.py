bf25b2ca96c2c1c1e0516734fe5f0647f2d039f8
Tamani Phiri <tamanigabriel0@gmail.com>
2023-05-05 18:21:48 +0200

    more

--- Travis.py
+++ Travis.py
@@ -9,13 +9,14 @@ machine=pyttsx3.init()
 
 def talk(text):
     machine.say(text)
-    
+
 machine.runAndWait()
-    
+
 def input_instruction():
     global instruction
     try:
         with aa.Microphone() as origin:
+
                 print("listenning now")
                 speech = listener.listen(origin)
                 instruction=listener.recognize_google(speech)
@@ -23,11 +24,11 @@ def input_instruction():
                 if "Travis" in instruction:
                         instruction=instruction.replace("Travis","")
                         print(instruction)
-                
+
     except:
         pass
     return instruction
-        
+
 def play_Travis():
 
     instruction = input_instruction()
@@ -36,26 +37,25 @@ def play_Travis():
         song=instruction.replace('play', "")
         talk("playing"+ song)
         pywhatkit.playsonyt(song)
-        
+
     elif 'time' in instruction:
         time= datetime.datetime.now().strftime('%I:%M&p')
         talk('Current time'+ time)
-        
+
     elif 'date' in instruction:
         date=datetime.datetime.now().strftime('%d /%m /%Y')
         talk("Today's date"+ date)
-        
+
     elif 'how are you' in instruction:
         talk('I am fine , how bout you?')
-        
+
     elif 'who is' in instruction:
         human=instruction.replace('who is', "")
         info=wikipedia.summary(human,1)
         print(info)
         talk(info)
-        
+
     else:
         talk('Please repeat')
 
 play_Travis()
-        
